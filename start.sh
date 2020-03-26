32e2cffdcf05b815e65d64876a7d2a5270d1ccd4
MARLON IVAN GUALDRON DIAZ <marlon@localhost.localdomain>
2020-03-25 23:56:27 -0500

    ajuste con otro server BD

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -f /var/www/html/moodle/config.php ]; then
   #mysql has to be started this way as it doesn't work to call from /etc/init.d
-  /usr/bin/mysqld_safe & 
+  #/usr/bin/mysqld_safe & 
   sleep 10s
   # Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
   MOODLE_DB="moodle"
@@ -15,11 +15,11 @@ if [ ! -f /var/www/html/moodle/config.php ]; then
   echo moodle password: $MOODLE_PASSWORD
   echo ssh root password: $SSH_PASSWORD
   echo root:$SSH_PASSWORD | chpasswd
-  echo $MYSQL_PASSWORD > /mysql-root-pw.txt
-  echo $MOODLE_PASSWORD > /moodle-db-pw.txt
-  echo $SSH_PASSWORD > /ssh-pw.txt
+  echo $MYSQL_PASSWORD > ~/mysql-root-pw.txt
+  echo $MOODLE_PASSWORD > ~/moodle-db-pw.txt
+  echo $SSH_PASSWORD > ~/ssh-pw.txt
 
-  sed -e "s/pgsql/mysqli/
+  sudo sed -e "s/pgsql/mysqli/
   s/username/moodle/
   s/password/$MOODLE_PASSWORD/
   s/example.com/$VIRTUAL_HOST/
@@ -27,8 +27,8 @@ if [ ! -f /var/www/html/moodle/config.php ]; then
 
   sed -i 's/PermitRootLogin without-password/PermitRootLogin Yes/' /etc/ssh/sshd_config
 
-  chown www-data:www-data /var/www/html/moodle/config.php
-  chmod 777 /var/www/html/moodle/config.php
+  sudo chown www-data:www-data /var/www/html/moodle/config.php
+  sudo chmod 777 /var/www/html/moodle/config.php
 
   mysqladmin -u root password $MYSQL_PASSWORD
   mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
