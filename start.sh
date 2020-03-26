ef5e588a86820edf90476d03314586a48165c114
MARLON IVAN GUALDRON DIAZ <marlon@localhost.localdomain>
2020-03-26 00:10:16 -0500

    ajustes isn mysql

--- start.sh
+++ start.sh
@@ -14,10 +14,10 @@ if [ ! -f /var/www/html/moodle/config.php ]; then
   echo mysql root password: $MYSQL_PASSWORD
   echo moodle password: $MOODLE_PASSWORD
   echo ssh root password: $SSH_PASSWORD
-  echo root:$SSH_PASSWORD | chpasswd
-  echo $MYSQL_PASSWORD > ~/mysql-root-pw.txt
-  echo $MOODLE_PASSWORD > ~/moodle-db-pw.txt
-  echo $SSH_PASSWORD > ~/ssh-pw.txt
+  sudo echo root:$SSH_PASSWORD | chpasswd
+  sudo  echo $MYSQL_PASSWORD > /mysql-root-pw.txt
+  sudo echo $MOODLE_PASSWORD > /moodle-db-pw.txt
+  sudo echo $SSH_PASSWORD > /ssh-pw.txt
 
   sudo sed -e "s/pgsql/mysqli/
   s/username/moodle/
@@ -30,10 +30,10 @@ if [ ! -f /var/www/html/moodle/config.php ]; then
   sudo chown www-data:www-data /var/www/html/moodle/config.php
   sudo chmod 777 /var/www/html/moodle/config.php
 
-  mysqladmin -u root password $MYSQL_PASSWORD
-  mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE moodle; GRANT ALL PRIVILEGES ON moodle.* TO 'moodle'@'localhost' IDENTIFIED BY '$MOODLE_PASSWORD'; FLUSH PRIVILEGES;"
-  killall mysqld
+#  mysqladmin -u root password $MYSQL_PASSWORD
+#  mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+#  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE moodle; GRANT ALL PRIVILEGES ON moodle.* TO 'moodle'@'localhost' IDENTIFIED BY '$MOODLE_PASSWORD'; FLUSH PRIVILEGES;"
+#  killall mysqld
 fi
 # start all the services
 /usr/local/bin/supervisord -n
